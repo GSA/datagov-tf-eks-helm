d17d471ca51e72af40dd83b2e0081c68649a858a
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-07-02 17:24:03 -0700

    Changes for Bret's personal testing domain and AWS ID

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "map_users" {
     groups   = list(string)
   }))
   default = [{
-    userarn = "arn:aws:iam::774350622607:user/auditor1",
+    userarn = "arn:aws:iam::503767083315:user/auditor1",
     username = "auditor1",
     groups = ["audit-team"]
   }]
